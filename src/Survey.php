abdb14b94436e8c47c8beea4fab45445d4ed9610
ccwrc <j.muszelik@gmail.com>
2017-04-24 17:52:22 +0200

    add class survey

---
+++ src/Survey.php
@@ -0,0 +1,24 @@
+<?php
+/* Ankieta:
+
+    1.Ma mieć własną nazwę, unikatowy link.
+    2.Ma implementować następujące metody:
+
+    zwracającą listę pytań dla danej ankiety,
+    zwracanie nazwy,
+    zmiana nazwy,
+    zapisanie zmian do bazy danych.
+
+    3.Ma implementować następujące statyczne metody:
+
+    stworzenie nowej ankiety,
+    wczytanie ankiety o podanym id z bazy danych,
+    usunięcie ankiety o podanym id z bazy danych. */
+
+class Survey {
+    private $surveyName;
+    private $surveyLink;
+    private $surveyId;
+    
+}
+
